09cd797ec9a6946af3f107a50bd55e9b6ef8cf40
INDRASHIS PAUL <indrashis985@gmail.com>
2020-12-08 00:07:34 +0530

    Updated seaborn plot

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
